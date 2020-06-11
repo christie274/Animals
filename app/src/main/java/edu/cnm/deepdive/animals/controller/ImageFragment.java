6386687fa5997386c2d744e701fdc8c39288a611
Christie Ryan <cryan11@cnm.edu>
2020-06-11 17:10:07 -0600

    Version 5 completed

--- app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
@@ -1,12 +1,6 @@
 package edu.cnm.deepdive.animals.controller;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
@@ -20,21 +14,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.squareup.picasso.Picasso;
-import edu.cnm.deepdive.animals.BuildConfig;
 import edu.cnm.deepdive.animals.MainViewModel;
 import edu.cnm.deepdive.animals.R;
 import edu.cnm.deepdive.animals.model.Animal;
-import edu.cnm.deepdive.animals.service.AnimalService;
-import java.io.IOException;
 import java.util.List;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ImageFragment extends Fragment implements OnItemSelectedListener {
 
@@ -44,7 +29,7 @@ public class ImageFragment extends Fragment implements OnItemSelectedListener {
 
   private Spinner spinner;
   private List<Animal> animals;
-  private int selectedAnimals = -1;
+  private int selectedAnimal = -1;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -75,15 +60,15 @@ public class ImageFragment extends Fragment implements OnItemSelectedListener {
       adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
       spinner.setAdapter(adapter);
       this.animals = animals;
-      if (selectedAnimals >= 0) {
+      if (selectedAnimal >= 0) {
         updateSelection();
       }
     });
 
     viewModel.getSelectedItem().observe(getViewLifecycleOwner(), (item) -> {
 
-      if (item != selectedAnimals) {
-        selectedAnimals = item;
+      if (item != selectedAnimal) {
+        selectedAnimal = item;
         if (animals != null) {
           updateSelection();
 
@@ -93,8 +78,8 @@ public class ImageFragment extends Fragment implements OnItemSelectedListener {
   }
 
   private void updateSelection() {
-    spinner.setSelection(selectedAnimals);
-    Picasso.get().load(animals.get(selectedAnimals).getUrl()).into(imageView);
+    spinner.setSelection(selectedAnimal);
+    Picasso.get().load(animals.get(selectedAnimal).getUrl()).into(imageView);
   }
 
 
